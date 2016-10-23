bcf6f5ec96cea366b5f7ec362ca609ffac158213
Derrick Hathaway <derr@me.com>
2016-10-22 22:08:47 -0600

    Initial Commit

---
+++ playbot/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  playbot
+//
+//  Created by Derrick Hathaway on 10/22/16.
+//  Copyright © 2016 Derrick Hathaway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
